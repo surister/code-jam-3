343519a8531eb5f677bf0e51b19ff8e9853a97d0
surister <surister98@gmail.com>
2018-10-22 19:21:55 +0200

    Physics class

--- project/sprites/dev_character.py
+++ project/sprites/dev_character.py
@@ -2,21 +2,31 @@ import pygame
 from project.constants import Color, HEIGHT, WIDTH
 
 
-class Character(pygame.sprite.Sprite):
-    def __init__(self, game):
+class Physics:
+    """
+    Sprite example
+
+    class Character(Test, pygame.sprite.Sprite):
+        def __init__(self, game):
+            super().__init__()
+            self.game = game
+            self.add(self.game.all_sprites)
+            self.image = pygame.Surface((50, 50))
+            self.rect = self.image.get_rect()
+            self.image.set_colorkey(Color.green)
+
+            self.acc = pygame.Vector2(0, 0)
+            self.vel = pygame.Vector2(0, 0)
+            self.pos = pygame.Vector2(200, 200)
+
+            self.friction = 1
+    """
+
+    def __init__(self):
         super().__init__()
-        self.game = game
-        self.add(self.game.all_sprites)
-        self.image = pygame.Surface((50, 50))
-        self.rect = self.image.get_rect()
-        self.image.set_colorkey(Color.green)
+        self.friction = 0.1
 
-        self.acc = pygame.Vector2(0, 0)
-        self.vel = pygame.Vector2(0, 0)
-        self.pos = pygame.Vector2(200, 200)
-        self.friction = -0.5
-
-    def update(self)-> None:
+    def update(self) -> None:
 
         self.key = pygame.key.get_pressed()
         # Can't detect UP and Down key??
@@ -32,7 +42,7 @@ class Character(pygame.sprite.Sprite):
 
         # self.acc += self.vel * self.friction
 
-        self.vel += self.acc
+        self.vel += self.acc * self.friction
 
         if self.vel.x > 10:
             self.vel.x = 10
@@ -55,3 +65,22 @@ class Character(pygame.sprite.Sprite):
             self.pos.x = 0
         if self.pos.x < 0:
             self.pos.x = WIDTH
+
+
+class Character(Physics, pygame.sprite.Sprite):
+    """
+    Sprite made for the sake of testing.
+    """
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.add(self.game.all_sprites)
+        self.image = pygame.Surface((50, 50))
+        self.rect = self.image.get_rect()
+        self.image.set_colorkey(Color.green)
+
+        self.acc = pygame.Vector2(0, 0)
+        self.vel = pygame.Vector2(0, 0)
+        self.pos = pygame.Vector2(200, 200)
+
+        self.friction = 1
