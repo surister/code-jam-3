c30ab53dc2444fa6d3d013afbd1cf3231e49251e
Jannes Jonkers <jjonkers2@gmail.com>
2018-10-22 21:00:55 +0200

    Moved control logic to player character from physics

--- project/sprites/dev_character.py
+++ project/sprites/dev_character.py
@@ -28,18 +28,6 @@ class Physics:
 
     def update(self) -> None:
 
-        self.key = pygame.key.get_pressed()
-        # Can't detect UP and Down key??
-        self.acc.y = self.acc.x = 0
-        if self.key[pygame.KEYUP] or self.key[pygame.K_w]:
-            self.acc.y = -1
-        if self.key[pygame.KEYDOWN] or self.key[pygame.K_s]:
-            self.acc.y = 1
-        if self.key[pygame.K_LEFT] or self.key[pygame.K_a]:
-            self.acc.x = -1
-        if self.key[pygame.K_RIGHT] or self.key[pygame.K_d]:
-            self.acc.x = 1
-
         # self.acc += self.vel * self.friction
 
         self.vel += self.acc * self.friction
--- project/sprites/player_character.py
+++ project/sprites/player_character.py
@@ -1,4 +1,18 @@
 from project.sprites.character import Character
 
 class PlayerCharacter(Character):
-    pass
+    #TODO determine base stats for the character
+
+    def update(self) -> None:
+        self.key = pygame.key.get_pressed()
+        
+        # Can't detect UP and Down key??
+        self.acc.y = self.acc.x = 0
+        if self.key[pygame.KEYUP] or self.key[pygame.K_w]:
+            self.acc.y = -1
+        if self.key[pygame.KEYDOWN] or self.key[pygame.K_s]:
+            self.acc.y = 1
+        if self.key[pygame.K_LEFT] or self.key[pygame.K_a]:
+            self.acc.x = -1
+        if self.key[pygame.K_RIGHT] or self.key[pygame.K_d]:
+            self.acc.x = 1
