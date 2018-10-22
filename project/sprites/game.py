183e4370e0b65fa49203d1fa50f43516e78eb8a9
surister <surister98@gmail.com>
2018-10-22 14:51:34 +0200

    Add basic game.py file with Game class that serves as a basic boilerplate for the project

---
+++ project/sprites/game.py
@@ -0,0 +1,52 @@
+import pygame
+from project.contants import WIDTH, HEIGHT, FPS
+
+
+class Game:
+    """
+    Main Game class
+    """
+    def __init__(self):
+        self.running = True
+        self.playing = True
+
+        self.screen = pygame.display.set_mode((WIDTH, HEIGHT))
+        self.clock = pygame.time.Clock()
+
+        pygame.init()
+        pygame.display.set_caption('Game in development')
+
+    def new(self):
+        """
+        Every time a new game starts
+        """
+        self._run()
+
+    def _run(self):
+
+        while self.playing:
+            self.clock.tick(FPS)
+            self._events()
+            self._update()
+            self._draw()
+
+    def _events(self):
+        """
+        Every event will be registered here
+        """
+        pass
+
+    def _update(self):
+        """
+        Every sprite's update will be registered here
+        """
+        pass
+
+    def _draw(self):
+        """
+        Everything we draw to the screen will be done here
+
+        Don't forget that we always draw first then -> pygame.display.flip()
+        """
+        pass
+
