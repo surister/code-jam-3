369ab3e921004daad1f40479ddfc0cc18f254b5e
surister <surister98@gmail.com>
2018-10-22 18:55:08 +0200

    Add basic character sprite with physics to be used as template for other sprites

---
+++ project/sprites/dev_character.py
@@ -0,0 +1,59 @@
+import pygame
+from project.constants import Color, HEIGHT, WIDTH
+
+
+class Character(pygame.sprite.Sprite):
+    def __init__(self, game):
+        super().__init__()
+        self.game = game
+        self.add(self.game.all_sprites)
+        self.image = pygame.Surface((50, 50))
+        self.rect = self.image.get_rect()
+        self.image.set_colorkey(Color.green)
+
+        self.acc = pygame.Vector2(0, 0)
+        self.vel = pygame.Vector2(0, 0)
+        self.pos = pygame.Vector2(200, 200)
+        self.friction = -0.5
+
+    def update(self)-> None:
+
+        self.key = pygame.key.get_pressed()
+        # Can't detect UP and Down key??
+        self.acc.y = self.acc.x = 0
+        if self.key[pygame.KEYUP] or self.key[pygame.K_w]:
+            self.acc.y = -1
+        if self.key[pygame.KEYDOWN] or self.key[pygame.K_s]:
+            self.acc.y = 1
+        if self.key[pygame.K_LEFT] or self.key[pygame.K_a]:
+            self.acc.x = -1
+        if self.key[pygame.K_RIGHT] or self.key[pygame.K_d]:
+            self.acc.x = 1
+
+        #self.acc += self.vel * self.friction
+
+        self.vel += self.acc
+
+        if self.vel.x > 10:
+            self.vel.x = 10
+        if self.vel.y > 10:
+            self.vel.y = 10
+        if self.vel.x < -10:
+            self.vel.x = -10
+        if self.vel.y < -10:
+            self.vel.y = -10
+        print(self.vel)
+        self.pos += self.vel  # plus something, friction?
+
+        self.rect.midbottom = self.pos
+
+        if self.pos.y > HEIGHT:
+            self.pos.y = 0
+        if self.pos.y < 0:
+            self.pos.y = HEIGHT
+        if self.pos.x > WIDTH:
+            self.pos.x = 0
+        if self.pos.x < 0:
+            self.pos.x = WIDTH
+
+        print(f'Vel: {self.vel} - Acc {self.acc}')
