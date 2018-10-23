73b528758ecef6090c29dcbcafaef6a882776b44
surister <surister98@gmail.com>
2018-10-23 10:19:13 +0200

    Add max speed, moved velocities variables to the correct place

--- project/sprites/dev_character.py
+++ project/sprites/dev_character.py
@@ -25,6 +25,10 @@ class Physics:
     def __init__(self):
         super().__init__()
         self.friction = 0.1
+        self.acc = pygame.Vector2(0, 0)
+        self.vel = pygame.Vector2(0, 0)
+        self.pos = pygame.Vector2(200, 200)
+        self.max_speed = 10
 
     def update(self) -> None:
 
@@ -44,14 +48,14 @@ class Physics:
 
         self.vel += self.acc * self.friction
 
-        if self.vel.x > 10:
-            self.vel.x = 10
-        if self.vel.y > 10:
-            self.vel.y = 10
-        if self.vel.x < -10:
-            self.vel.x = -10
-        if self.vel.y < -10:
-            self.vel.y = -10
+        if self.vel.x > self.max_speed:
+            self.vel.x = self.max_speed
+        if self.vel.y > self.max_speed:
+            self.vel.y = self.max_speed
+        if self.vel.x < -self.max_speed:
+            self.vel.x = -self.max_speed
+        if self.vel.y < -self.max_speed:
+            self.vel.y = -self.max_speed
 
         self.pos += self.vel  # plus something, friction?
 
@@ -79,9 +83,5 @@ class Character(Physics, pygame.sprite.Sprite):
         self.image = pygame.Surface((50, 50))
         self.rect = self.image.get_rect()
         self.image.set_colorkey(Color.green)
-
-        self.acc = pygame.Vector2(0, 0)
-        self.vel = pygame.Vector2(0, 0)
-        self.pos = pygame.Vector2(200, 200)
 
         self.friction = 1
