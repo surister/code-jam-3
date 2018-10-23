023c97b6722cbe6e1105cf6562ced3b58277a750
surister <surister98@gmail.com>
2018-10-23 10:59:48 +0200

    Class tweaks

--- project/menus/home.py
+++ project/menus/home.py
@@ -9,17 +9,20 @@ class Home:
         self.start_button = pygame.Rect(0, 0, 400, 100)
         self.start_button.midtop = (WIDTH/2, HEIGHT/7)
         self.start_button_text = "START"
+        self.in_horizontal = False
+        self.in_vertical = False
 
-    def draw(self, x: int, y: int):
+    def draw(self, x: int, y: int)-> None:
         self.screen.fill(Color.dark_blue)
 
-        in_horizontal = self.start_button.left + self.start_button.width > x > self.start_button.left
-        in_vertical = self.start_button.top + self.start_button.height > y > self.start_button.top
+        self.in_horizontal = self.start_button.left + self.start_button.width > x > self.start_button.left
+        self.in_vertical = self.start_button.top + self.start_button.height > y > self.start_button.top
 
-        if in_horizontal and in_vertical:
+        if self.in_horizontal and self.in_vertical:
             self._draw_start_button(hover=True)
         else:
             self._draw_start_button()
+        pygame.display.flip()
 
     def _draw_text(self, size: int, text: str, color: Color, cords: tuple):
         font = pygame.font.Font(self.font, size)
@@ -28,7 +31,7 @@ class Home:
         text_rect.midtop = cords
         self.screen.blit(text_surface, text_rect)
 
-    def _draw_start_button(self, hover=False)->None:
+    def _draw_start_button(self, hover=False)-> None:
 
         color = Color.dark_yellow
 
