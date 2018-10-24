14c6c9643defc48f865eceaae73a98db725534b0
Jannes Jonkers <jjonkers2@gmail.com>
2018-10-24 17:39:43 +0200

    removed npc class

--- project/sprites/non_player_character.py
+++
@@ -1,8 +0,0 @@
-from project.sprites.character import Character
-
-
-class NonPlayerCharacter(Character):
-    """ Base class for all NPC's """
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        self.add(self.game.enemy_sprites)
