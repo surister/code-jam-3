034bb67d25eac74c35391bda2ac3302b7f1d0ce0
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2018-10-26 22:45:07 +0300

    removing convert_alpha()

--- project/sprites/sheet.py
+++ project/sprites/sheet.py
@@ -8,7 +8,7 @@ class Sheet:
 
     def get_image(self, x, y, width, height, alpha=False):
         image = pg.Surface((width, height))
-        image.blit(self.spritesheet, (0, 0), (x, y, width, height)).convert_alpha()
+        image.blit(self.spritesheet, (0, 0), (x, y, width, height))
 
         if alpha:
             return image.convert_alpha()
