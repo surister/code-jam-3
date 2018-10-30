ac13ca8e8712e94ed7aa488a69f6dd3f505aae3c
Vladislav Mihov <37806520+skilldeliver@users.noreply.github.com>
2018-10-30 22:30:32 +0200

    doc-stringing the sheet

--- project/ui/sheet.py
+++ project/ui/sheet.py
@@ -2,11 +2,21 @@ import pygame as pg
 
 
 class Sheet:
-
+    """
+    Represents tool for extracting sprites from spritesheet.
+    """
     def __init__(self, sheet_path):
+        """
+        Constructur for the sheet tool.
+        Loading the spritesheet.
+        """
         self.spritesheet = pg.image.load(sheet_path).convert_alpha()
 
     def get_image(self, x, y, width, height, alpha=False):
+        """
+        Extracts sprite of given point (x, y) (left, top) and width and height.
+        alpha boolean keyword argument for converting the sprite in alpha or non-alpha.
+        """
         image = pg.Surface((width, height))
         image.blit(self.spritesheet, (0, 0), (x, y, width, height))
 
